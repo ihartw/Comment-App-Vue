0aa14378e4e6d06a959fff48fc1ef6d2f2056e48
Isaac <isaacmain@Isaacs-MacBook-Air.local>
2019-01-14 22:09:45 -0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	assetsDir: undefined,
 	baseUrl: undefined,
+	publicPath: process.env.NODE_ENV === 'production' ? '/Comment-App-Vue/' : '/',
 	outputDir: undefined,
 	runtimeCompiler: undefined,
 	productionSourceMap: undefined,
